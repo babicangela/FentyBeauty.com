946ac69a9d020b549f1d01c8ab0949bcfa60e618
babicangela <94862335+babicangela@users.noreply.github.com>
2022-02-11 13:09:35 +0100

    Update SearchTest.java

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,189 +1,69 @@
+import com.aventstack.extentreports.model.Report;
 import org.testng.Reporter;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 
-public class ShoppingTest extends BaseTest {
-
+public class SearchTest extends BaseTest {
 
     /**
-     * Add and remove item from shopping cart page
+     * Search and add item in the shopping cart
 
       Test steps:
-      1. Navigate to https://fentybeauty.com/, accept cookies and welcome message
-      2. Click on Shop page link from the header navigation menu
-      3. Add to cart "Glossy Posse Bomb Collection"
-      4. Add to cart "Match Stix Contour Skinstick:Lunar New Year Edition"
-      5. Click on Shopping cart button from the header navigation menu, then click on View button in the shopping cart overview
-      6. Click on remove button
+      1. Navigate to https://fentybeauty.com/
+      2. Accept cookies and welcome message
+      3. Click on search button from the header navigation menu
+      4. Type in the item name and press enter
+      5. Click on Royal Icing from the displayed search result list
+      6. Click on Rosé Rave from the Swatch selector list
+      7. Click add to cart
 
       Expected results:
-      2. Verify that the shopping cart is empty
-      3. Verify that the shopping cart overview wrapper containing selected item slides out on the right side of the page
-      3. Verify that the number 1 is displayed in the shopping cart badge
-      4. Verify that the shopping cart overview wrapper containing 2 selected items slides out on the right side of the page
-      4. Verify that the number 2 is displayed in the shopping cart badge
-      5. Verify that the shopping cart overview wrapper containing 2 selected items slides out on the right side of the page
-      5. Verify the Shopping cart page URL and that the number of items displayed in the shopping cart is 2 items
-      6. Verify that the number of items left in the shopping cart is 1 item
-      6. Verify that number 1 is displayed on the shopping cart badge
+      4. Verify that the user is on a search result page and the title is "SEARCH RESULTS FOR "HIGHLIGHTER DIAMOND"
+      5. Verify that the user is navigated to the Royal Icing products page URL
+      6. Verify that Rosé Rave shade name in displayed in the shade selector dropdown, and that the user is on Rosé Rave products page URL
+      7. Verify that the shopping cart overview wrapper containing selected item slides out on the right side of the page
+      7. Verify that the number 1 is displayed in the shopping cart badge
      */
 
-
-    @Test(priority = 1)
-    public void addAndRemoveItemsFromShoppingCartPage() {
+    @Test
+    public void searchAndAddItemToCart() {
         driver = openChromeDriver();
 
         try {
             BasePage basePage = new BasePage(driver);
             basePage
                     .clickOnCookiesAcceptButton()
-                    .clickAcceptWelcomeMessage();
+                    .clickAcceptWelcomeMessage()
+                    .clickOnSearchButon();
 
-            ShopPage shopPage = basePage.clickOnShopButtonLInk();
-            Reporter.log("Verify that the shopping cart is empty",true);
-            assert shopPage.getNumberOfItemsInShoppingCartFromBadge().equals("0"): "Error. Shopping cart badge number should be 0, but is " + shopPage.shoppingCartBadgeNumber.getText() + " instead";
+            SearchPage searchPage = basePage.enterTextInSearchInputField(StringsPage.DIAMOND);
 
-            shopPage
-                    .clickAddToCartGlossyPosseBombCollection();
-
-            sleep();
-            Reporter.log("Verify that the number 1 is displayed in the shopping cart badge",true);
-            assert isElementPresent(shopPage.headerCartOverviewWrapper):"Error. Header cart overview wrapper NOT displayed on page";
-            assert shopPage.getNumberOfItemsInShoppingCartFromBadge().equals("1"): "Error. Shopping cart badge number should be 1, but is " + shopPage.shoppingCartBadgeNumber.getText();
-
-            shopPage.waitForElement(shopPage.matchStixContourSkinstick);
-            shopPage.clickAddToCartMatchStixContourSkindStick();
+            log.info("Verify that the title is SEARCH RESULTS FOR HIGHLIGHTER DIAMOND");
+            Reporter.log("Verify that the tittle is SEARCH RESULTS FOR HIGHLIGHTER DIAMOND");
+            assert searchPage.searchResults.getText().contains(StringsPage.DIAMOND) : "Error. The tittle text should be " + StringsPage.DIAMOND + " but is " + searchPage.searchResults.getText() + " instead" ;
+            assert isCurrentURLEqualTo(StringsPage.SEARCH_PAGE_URL) : "Error. The user should be on " + StringsPage.SEARCH_PAGE_URL + " but is on " + driver.getCurrentUrl() + " instead ";
 
             sleep();
-            Reporter.log("Verify that the number 2 is displayed in the shopping cart badge", true);
-            assert isElementPresent(shopPage.headerCartOverviewWrapper):"Error. Header cart overview wrapper not displayed on page";
-            assert shopPage.getNumberOfItemsInShoppingCartFromBadge().equals("2"): "Error. Shopping cart badge number should be 2, but is " + shopPage.shoppingCartBadgeNumber.getText();
+            ProductsPage productsPage = searchPage.clickOnRoyalIcing(StringsPage.ROYAL_ICING);
+            assert isCurrentURLEqualTo(StringsPage.ROYAL_ICING_PAGE_URL) : "Error. The user should be on " + StringsPage.ROYAL_ICING + " but is on " + driver.getCurrentUrl() + " instead";
 
+            productsPage.clickOnRoseRave(StringsPage.ROSE_RAVE);
             sleep();
-            ShoppingCartPage shoppingCartPage = shopPage.clickOnShoppingCartHeaderButton().clickOnViewBagButton();
-
-            assert isElementPresent(shoppingCartPage.headerCartOverviewWrapper):"Error. Header cart overview wrapper NOT displayed";
-            assert isCurrentURLEqualTo(StringsPage.SHOPPING_CART_PAGE_URL) : "Error. The user should be on " + StringsPage.SHOPPING_CART_PAGE_URL + " but is on " + driver.getCurrentUrl() + " instead";
-            Reporter.log("Verify that the number of items displayed in the shopping cart is 2 items", true);
-            assert shoppingCartPage.getNumberOfItemsInShoppingCartFromShoppingCart().equals("2 items"): "Error. Shopping cart items number should be 2, but is " + shoppingCartPage.shoppingCartItemNumber.getText();
-
-//          sleep();
-            shoppingCartPage.clickOnRemoveItemFromCart();
-            sleep();
-
-            Reporter.log("Verify that the number of items left in the shopping cart is 1 item",true);
-            assert shoppingCartPage.getNumberOfItemsInShoppingCartFromShoppingCart().equals("1 items"): "Error. Shopping cart items number should be 1 items, but is " + shoppingCartPage.shoppingCartItemNumber.getText();
-            Reporter.log("Verify that number 1 is displayed on the shopping cart badge", true);
-            assert shoppingCartPage.shoppingCartBadgeNumber.getText().equals("1"): "Error. Shopping cart badge number should be 1, but is " + shopPage.shoppingCartBadgeNumber.getText();
-
-        } finally {
-
-            driver.quit();
-        }
-    }
-
-
-    /** Filter, Add and Increase The Item Number, and proceed to Checkout
-     *
-     Test steps:
-     1. Navigate to https://fentybeauty.com/, accept cookies and welcome message
-     2. Click on Shop page link from the header navigation menu
-     3. Click on Show filters button
-     4. Click on Natural checkbox
-     5. Click on Liquid checkbox
-     6. Click Apply filters button
-     7. Click Hide filters button
-     8. Click on "Eaze Drop Blurring Skin Tint 11"
-     9. Click Add to cart
-     10. Click on Shopping cart button from the header navigation menu, then click on View button from the Header cart overview wrapper
-     11. Click on the + button
-     12. Click on the Checkout button
-
-     Expected results:
-     2. Verify that the number of items on Shop page before clicking on apply button is 111
-     6. Verify that the number of items on Shop page after clicking on apply button is 25
-     8. The user is navigated to the "Eaze Drop Blurring Skin Tint 11" products page. Verify the URL.
-     9. Verify that the shopping cart overview wrapper containing selected items slides out on the right side of the page
-     9. Verify that the number 1 is displayed on the shopping cart badge
-     10. Verify that the shopping cart overview wrapper containing selected item slides out on the right side of the page
-     10. Verify the  Shopping cart page URL
-     10. Verify that the number of items in the shopping cart is 1items
-     11. Verify that the number of items in shopping cart after clicking on + button is 2items
-     12. Verify that the user is on the Checkout page URL, and  Fenty Beauty logo is displayed on page
-
-     */
-
-
-    @Test(priority = 2)
-    public void filterAndAddItemThenIncreaseTheNumberInTheShoppingCart(){
-        driver = openChromeDriver();
-
-        try {
-            BasePage basePage = new BasePage(driver);
-            basePage
-                    .clickOnCookiesAcceptButton()
-                    .clickAcceptWelcomeMessage();
-            ShopPage shopPage = basePage.clickOnShopButtonLInk();
-            shopPage
-                    .clickOnShowFiltersButton();
-            Reporter.log("Verify that the number of items on Shop page before clicking on apply button is 115", true);
-            assert shopPage.shopPageItemNumber.getText().equals("114 ITEMS"): "Error. Shop page items number should be 115 ITEMS, but instead is " + shopPage.shopPageItemNumber.getText();
-
-            shopPage
-                    .clickOnNaturalFilterCheckBox()
-                    .clickOnLiquidFilterCheckBox()
-                    .clickOnApplyFiltersButton()
-                    .clickOnHideFilterButton();
-
-            Reporter.log("Verify that the number of items on Shop page after applying selected filters is 75",true);
-            sleep();
-            assert shopPage.shopPageItemNumber.getText().equals("75 ITEMS"): "Error. Shop page items number should be 75 ITEMS, but instead is " + shopPage.shopPageItemNumber.getText();
-
-
-            sleep();
-            ProductsPage productsPage = shopPage.clickOnEazeDropBlurringSkinTint11();
-
-            sleep();
-            Reporter.log("Verify that the user is navigated to the Eaze Drop Blurring Skin Tint 11 page URL", true);
-            assert isCurrentURLEqualTo(StringsPage.PRODUCTS_PAGE_URL):"Error. The user should be on " + StringsPage.PRODUCTS_PAGE_URL + " but is on " + driver.getCurrentUrl() + " instead";
+            assert productsPage.shadeSelectorDropDown.getText().contains(StringsPage.ROSE_RAVE): "Error. Displayed shade name should be" + StringsPage.ROSE_RAVE + " but is" + productsPage.shadeSelectorDropDown.getText();
+            assert isCurrentURLEqualTo(StringsPage.ROSE_RAVE_PAGE_URL):"Error. The user should be on " + StringsPage.ROSE_RAVE + " but is on " + driver.getCurrentUrl() + " instead";
 
             productsPage
                     .clickOnAddToCart();
+            assert isElementPresent(productsPage.headerCartOverviewWrapper):"Error. Header cart overview wrapper NOT present on page";
 
             sleep();
+            log.info("Verify that number 1 is displayed in the shopping cart badge");
+            Reporter.log("Verify that number 1 is displayed in the shopping cart badge");
+            assert productsPage.shoppingCartBadgeNumber.getText().contains("1") : "Error. Number 1 should be displayed on the shopping cart badge number but instead " + productsPage.shoppingCartBadgeNumber.getText() + " is displayed";
 
-            Reporter.log("Verify that the Shopping cart header overview containing selected item slides out on the right side of the page, and that the number 1 is displayed on the shopping bag badge after clicking add to cart", true);
-            assert isElementPresent(shopPage.headerCartOverviewWrapper):"Error. Header cart overview wrapper NOT displayed on page";
-            assert isElementPresent(shopPage.shoppingCartBadgeNumber): "Error, shopping cart badge number NOT displayed";
-            assert shopPage.shoppingCartBadgeNumber.getText().contains("1"): "Error. Shopping cart badge number should be 1, but instead is " + shopPage.shoppingCartBadgeNumber.getText() + " instead";
+        } finally {
 
-            sleep();
-            ShoppingCartPage shoppingCartPage = shopPage.clickOnShoppingCartHeaderButton().clickOnViewBagButton();
-
-            sleep();
-            assert  isElementPresent(shoppingCartPage.headerCartOverviewWrapper):"Error. Header cart overview wrapper not displayed on page";
-            assert isCurrentURLEqualTo(StringsPage.SHOPPING_CART_PAGE_URL):"Error. The user should be on " + StringsPage.SHOPPING_CART_PAGE_URL + " but is on" + driver.getCurrentUrl() + " instead";
-
-            Reporter.log("Verify that the number of items in the shopping cart is 1 items",true);
-            assert shoppingCartPage.isShoppingCartItemsNumberPresent():"Error. The number of items in shopping cart NOT displayed";
-            assert shoppingCartPage.shoppingCartItemNumber.getText().equals("1 items"): "Error. Shopping cart item number should be 1, but instead is " + shoppingCartPage.shoppingCartItemNumber.getText();
-
-            shoppingCartPage
-                    .clickOnAddMoreButton();
-            sleep();
-            Reporter.log("Verify that the number of items in shopping cart after clicking add more button is 2 items", true);
-            assert shoppingCartPage.shoppingCartItemNumber.getText().equals("2 items"): "Error. Shopping cart item number should be 2, but instead is " + shoppingCartPage.shoppingCartItemNumber.getText();
-
-            CheckoutPage checkoutPage = shoppingCartPage.clickOnCheckoutButton();
-
-            sleep();
-            assert isCurrentURLEqualTo(StringsPage.CHECKOUT_PAGE_URL):"Error. The user should be on " + StringsPage.CHECKOUT_PAGE_URL + " but is on " + driver.getCurrentUrl() + " instead";
-            Reporter.log("Verify that Fenty Beauty logo is displayed on page", true);
-            assert isElementPresent(checkoutPage.fentyBeautyLogo):"Error. Fenty Beauty Logo NOT displayed on page";
-
-        }finally{
-
-        }  driver.quit();
+        }
+        driver.quit();
     }
 }
-
