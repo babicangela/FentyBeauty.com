688dadb2b49d662ceaa12e9ee611b69aa75112b4
babicangela <94862335+babicangela@users.noreply.github.com>
2022-02-12 11:36:46 +0100

    Update SignInTest.java

--- src/test/java/SignInTest.java
+++ src/test/java/SignInTest.java
@@ -9,16 +9,21 @@ public class SignInTest extends BaseTest {
  * Sign in with valid credentials
 
   Test steps:
-  1. Navigate to https://fentybeauty.com/, accept cookies and welcome message
-  2. Click on account page icon from the header navigation menu
-  3. Enter valid email in email input field
-  4. Enter valid password in password input field
-  5. Click on login button
+  1. Navigate to https://fentybeauty.com/
+  2. Accept cookies and welcome message
+  3. Click on account page icon from the header navigation menu
+  4. Enter valid email in email input field
+  5. Enter valid password in password input field
+  6. Click on login button
 
  Expected result:
-   2. Verify that the user is on the Signin page URL
-   5. Verify that the user is signed in and on the user account page
-   5. Verify that the users name is displayed on the user account page
+   1. The 'Accept cookies' and 'Welcome message' are displayed on screen
+   2. 'Accept cookies' and 'Welcome message' are no longer displayed on screen
+   3. Verify that the user is on the Signin page URL
+   4. Verify that 'email input field' is displayed on page
+   5. Verify that 'password input field' is displayed on page
+   6. Verify that the user is signed in and on the user account page
+   6. Verify that the users name is displayed on the user account page
  */
 
     @Test(priority = 1)
@@ -52,18 +57,23 @@ public class SignInTest extends BaseTest {
      * Sign in with valid credentials and log out
 
      Test steps:
-     1. Navigate to https://fentybeauty.com/, accept cookies and welcome message
-     2. Click on account page icon from the header navigation menu
-     3. Enter valid email in email input field
-     4. Enter valid password in password input field
-     5. Click on login button
-     6. Click on logout button on the user account page
+     1. Navigate to https://fentybeauty.com/
+     2. Accept cookies and welcome message
+     3. Click on account page icon from the header navigation menu
+     4. Enter valid email in email input field
+     5. Enter valid password in password input field
+     6. Click on login button
+     7. Click on logout button on the user account page
 
      Expected result:
-     2. Verify that the user is on sign in page URL
-     5. Verify that the user is signed in and on the user account page
-     5. Verify that the users name is displayed on the user account page
-     6. Verify that the user is on home page
+     1. The 'Accept cookies' and 'Welcome message' are displayed on screen
+     2. 'Accept cookies' and 'Welcome message' are no longer displayed on screen
+     3. Verify that the user is on the Signin page URL
+     4. Verify that 'email input field' is displayed on page
+     5. Verify that 'password input field' is displayed on page
+     6. Verify that the user is signed in and on the user account page
+     6. Verify that the users name is displayed on the user account page
+     7. Verify that the user is on home page
      */
 
     @Test(priority = 2)
@@ -102,15 +112,20 @@ public class SignInTest extends BaseTest {
      * Sign in with invalid email and valid password
 
      Test steps:
-     1. Navigate to https://fentybeauty.com/, accept cookies and welcome message
-     2. Click on account page icon from the header navigation menu
-     3. Enter invalid email in email input field
-     4. Enter valid password in password input field
-     5. Click on login button
+     1. Navigate to https://fentybeauty.com/
+     2. Accept cookies and welcome message
+     3. Click on account page icon from the header navigation menu
+     4. Enter invalid email in email input field
+     5. Enter valid password in password input field
+     6. Click on login button
 
      Expected results:
-     2. Verify that the user is on sign in page URL
-     5. Verify that the user is on sign in page and error message containing "*email is not valid" is displayed
+     1. The 'Accept cookies' and 'Welcome message' are displayed on screen
+     2. 'Accept cookies' and 'Welcome message' are no longer displayed on screen
+     3. Verify that the user is on the Signin page URL
+     4. Verify that 'email input field' is displayed on page
+     5. Verify that 'password input field' is displayed on page
+     6. Verify that the user is on sign in page and error message containing "*email is not valid" is displayed
      */
 
     @Test(priority = 3)
@@ -143,14 +158,18 @@ public class SignInTest extends BaseTest {
      * Sign in with valid email and empty password
 
      Test steps:
-     1. Navigate to https://fentybeauty.com/, accept cookies and welcome message
-     2. Click on account page icon from the header navigation menu
-     3. Enter valid email in email input field
-     4. Click on login button
+     1. Navigate to https://fentybeauty.com/
+     2. Accept cookies and welcome message
+     3. Click on account page icon from the header navigation menu
+     4. Enter valid email in email input field
+     5. Click on login button
 
      Expected result:
-     2. Verify that the user is on sign in page URL
-     4. Verify that error message containing "*password must be more than 5 characters" is displayed.
+     1. The 'Accept cookies' and 'Welcome message' are displayed on screen
+     2. 'Accept cookies' and 'Welcome message' are no longer displayed on screen
+     3. Verify that the user is on the Signin page URL
+     4. Verify that 'email input field' is displayed on page
+     5. Verify that error message containing "*password must be more than 5 characters" is displayed.
 
      */
 
@@ -177,4 +196,5 @@ public class SignInTest extends BaseTest {
         } finally {
 
         }driver.quit();
-    }}
+    }
+}
