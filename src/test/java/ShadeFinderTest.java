10b4718944c1d4370cd4a25ccbb102cb1c4458ad
babicangela <94862335+babicangela@users.noreply.github.com>
2022-02-12 11:34:49 +0100

    Update ShadeFinderTest.java

--- src/test/java/ShadeFinderTest.java
+++ src/test/java/ShadeFinderTest.java
@@ -6,19 +6,29 @@ public class ShadeFinderTest extends BaseTest{
 
     /**
      Test steps:
-      1. Navigate to https://fentybeauty.com/, accept cookies and welcome message
-      2. Click on find my shade button
-      3. Click on the MEDIUM/LEFT photo(the 5th photo in a row)
-      4. Click on BOTH button
-      5. Click on RARELY BURNS AND/OR TANS EASILY BUTTON
-      6. Click on PURPLE button
-      7. Click on GOLDEN OLIVE button
-      8. Click on the #300 Warm Golden(the 2nd photo in the row)
+      1. Navigate to https://fentybeauty.com/
+      2. Accept cookies and welcome message
+      3. Click on SHADE FINDER page link from header navigation menu
+      4. Click on find my shade button
+      5. Click on the MEDIUM/LEFT photo(the 5th photo in a row)
+      6. Click on BOTH button
+      7. Click on RARELY BURNS AND/OR TANS EASILY BUTTON
+      8. Click on PURPLE button
+      9. Click on GOLDEN OLIVE button
+      10. Click on the #300 Warm Golden(the 2nd photo in the row)
 
      Expected results:
-      2. The user is navigated to the Shade finder search result page with the results displayed on the page
-      8. Verify that the PRO FILT’R displayed shade number result is #300
-      8. Verify the Shade Finder search results page URL
+      1. The 'Accept cookies' and 'Welcome message' are displayed on screen
+      2. 'Accept cookies' and 'Welcome message' are no longer displayed on screen
+      3. The user is navigated to the Shade finder URL page, verify that 'FIND MY SHADE' button is present on page
+      4. Verify that 'SoftMate255' (5th photo in the displayed row) is displayed in the 'SELECT THE MODEL CLOSEST TO YOUR SKIN TONE TO FIND YOUR SHADE RANGE' options list
+      5. Verify that 'WHAT TONE OF JEWELRY DO YOU LOOK BEST IN' selection list is displayed on page
+      6. Verify that 'RARELY BURNS AND/OR TANS EASILY' button is displayed in the 'HOW DOES YOUR SKIN REACT TO THE SUN' selection list
+      7. Verify that 'PURPLE' button is displayed in the 'HOW DO THE VEINS ON YOUR INNER WRIST APPEAR' selection list
+      8. Verify that 'GOLDEN OLIVE' button is displayed in the 'HOW DOES YOUR SKIN APPEAR IN ITS MOST NATURAL STATE' selection list
+      9. Verify that '#300 WARM GOLDEN' photo is in the middle of the 'CHOOSE THE MODEL WHOSE SKIN TONE IS CLOSEST TO YOURS' selection list
+      10. Verify that the PRO FILT’R displayed shade number result is #300
+      10. Verify the Shade Finder search results page URL
      */
 
 
@@ -53,4 +63,5 @@ public class ShadeFinderTest extends BaseTest{
         }
         driver.quit();
     }
+
 }
