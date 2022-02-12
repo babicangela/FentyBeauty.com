b3ff782b5a9efd29935fec29188dee9ddca20894
babicangela <94862335+babicangela@users.noreply.github.com>
2022-02-12 10:12:26 +0100

    Add files via upload

---
+++ src/main/java/StringsPage.java
@@ -0,0 +1,38 @@
+public class StringsPage {
+
+        // Credentials
+        public static final String VALID_EMAIL = "QAProjekat@yandex.com";
+        public static final String VALID_PASSWORD = "QAProjekat2021";
+        public static final String INVALID_EMAIL = "QAProjekat@.com";
+
+        //    URLS
+        public static final String HOME_PAGE_URL = "https://fentybeauty.com/";
+        public static final String SIGNIN_PAGE_URL = "https://fentybeauty.com/account/login?form=login";
+        public static final String USER_ACCOUNT_PAGE_URL ="https://fentybeauty.com/account";
+        public static final String SHOP_PAGE_URL = "https://fentybeauty.com/collections/makeup-all";
+        public static final String SEARCH_PAGE_URL = "https://fentybeauty.com/pages/search-results?s=HIGHLIGHTER%20DIAMOND";
+        public static final String PRODUCTS_PAGE_URL = "https://fentybeauty.com/products/eaze-drop-blurring-skin-tint-11";
+        public static final String ROYAL_ICING_PAGE_URL = "https://fentybeauty.com/products/diamond-bomb-all-over-diamond-veil-royal-icing";
+        public static final String ROSE_RAVE_PAGE_URL = "https://fentybeauty.com/products/diamond-bomb-all-over-diamond-veil-rose-rave";
+        public static final String SHOPPING_CART_PAGE_URL = "https://fentybeauty.com/cart";
+        public static final String CHECKOUT_PAGE_URL = "https://fentybeauty.com/pages/international-checkout#Global-e_International_Checkout";
+        public static final String SHADE_FINDER_URL = "https://fentybeauty.com/pages/fenty-face-shade-finder";
+        public static final String SHADE_FINDER_RESUTLS_URL = "https://fentybeauty.com/pages/fenty-face-shade-finder-results?shade=300";
+        public static final String PRO_FILTR185_PRODUCTS_URL = "https://fentybeauty.com/products/pro-filtr-soft-matte-longwear-foundation-185";
+        public static final String PRO_FILTR300_PRODUCTS_URL = "https://fentybeauty.com/products/pro-filtr-soft-matte-longwear-foundation-300";
+
+        // Error messages
+        public static final String INVALID_EMAIL_LOGIN_MESSAGE = "*email is not valid";
+        public static final String INVALID_PASSWORD_LOGIN_MESSAGE = "Incorrect email or password.";
+        public static final String EMPTY_PASSWORD_MESSAGE = "*password must be more than 5 characters";
+
+        // Item Names
+        public static final String ROYAL_ICING = "Royal Icing";
+        public static final String SHADE_FINDER = "SHADE FINDER";
+        public static final String DIAMOND = "HIGHLIGHTER DIAMOND";
+        public static final String ROSE_RAVE = "Rosé Rave";
+
+}
+
+
+
